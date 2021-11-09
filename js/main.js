96b8c9f021bc93c453b609dbb5b86a0d9a0195ce
shastinmax <shastin_maksim@mail.ru>
2021-11-09 14:00:17 +0300

    Regex

--- js/main.js
+++ js/main.js
@@ -285,6 +285,7 @@ window.addEventListener("DOMContentLoaded", () => {
   //Slider
   const slides = document.querySelectorAll(".offer__slide"),
     prev = document.querySelector(".offer__slider-prev"),
+    slider = document.querySelector(".offer__slider"),
     next = document.querySelector(".offer__slider-next"),
     total = document.querySelector("#total"),
     current = document.querySelector("#current"),
@@ -312,11 +313,54 @@ window.addEventListener("DOMContentLoaded", () => {
     slide.style.width = width;
   });
 
+  slider.style.position = "relative";
+
+  const indicators = document.createElement("ol"),
+    dots = [];
+  indicators.classList.add("carousel-indicators");
+  indicators.style.cssText = `
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 15;
+  display: flex;
+  justify-content: center;
+  margin-right: 15%;
+  margin-left: 15%;
+  list-style: none;
+  `;
+  slider.append(indicators);
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("li");
+    dot.setAttribute("data-slide-to", i + 1);
+    dot.style.cssText = `
+    box-sizing: content-box;
+    flex: 0 1 auto;
+    width: 30px;
+    height: 6px;
+    margin-right: 3px;
+    margin-left: 3px;
+    cursor: pointer;
+    background-color: #fff;
+    background-clip: padding-box;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    opacity: .5;
+    transition: opacity .6s ease;
+    `;
+    if (i === 0) {
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+  }
+
   next.addEventListener("click", () => {
-    if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset === +width.replace(/\D/g, "") * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += +width.replace(/\D/g, "");
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -330,12 +374,19 @@ window.addEventListener("DOMContentLoaded", () => {
     } else {
       current.textContent = slideIndex;
     }
+    dots.forEach((dot) => (dot.style.opacity = ".5"));
+    dots[slideIndex - 1].style.opacity = "1";
   });
+
+  const replaceExpression = (expression) => {
+    return +expression.replace(/\D/g, "");
+  };
+
   prev.addEventListener("click", () => {
     if (offset === 0) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = replaceExpression(width) * (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= replaceExpression(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -344,11 +395,33 @@ window.addEventListener("DOMContentLoaded", () => {
     } else {
       slideIndex--;
     }
+
     if (slides.length < 10) {
       current.textContent = `0${slideIndex}`;
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach((dot) => (dot.style.opacity = ".5"));
+    dots[slideIndex - 1].style.opacity = "1";
+    dots.forEach((dot) =>
+      dot.addEventListener("click", (e) => {
+        const slideTo = e.target.getAttribute("data-slide-to");
+
+        slideIndex = slideTo;
+        offset = +replaceExpression(width) * (slideTo - 1);
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slides.length < 10) {
+          current.textContent = `0${slideIndex}`;
+        } else {
+          current.textContent = slideIndex;
+        }
+
+        dots.forEach((dot) => (dot.style.opacity = ".5"));
+        dots[slideIndex - 1].style.opacity = "1";
+      })
+    );
   });
   // showSlides(slideIndex);
 
